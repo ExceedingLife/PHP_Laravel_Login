5b58e574bde95ed1d5c7e21a5eb188667931fcc4
Andrew Harkins <ExtremelyExceeding@gmail.com>
2019-03-04 22:55:37 -0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// route to show the login form
+//Route::get('login', array('uses'=>'HomeController@showLogin'));
+// route to process the form auth.passwords.reset
+//Route::post('login', array('uses'=>'HomeController@doLogin'));
+
+// no email verification
+Auth::routes();
+
+// adding email verification
+Auth::routes(['verify' => true]);
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/changePassword', 'HomeController@showChangePasswordForm');
+
+// Route::get('/passwordChange', 'HomeController@showSetPasswordView');
+Route::get('/passwordChange', function(){
+    return view('auth.passwords.reset');
+});
